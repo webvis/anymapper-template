c2ee4687d43e6e4d8a000e838cee306158c37dde
nitaku <matteo.abrate@gmail.com>
2022-09-14 12:22:19 +0200

    simplified example (less stuff in POIs)

--- src/src/POI.svelte
+++ src/src/POI.svelte
@@ -1,6 +1,6 @@
 <script>
     import { zoom, current_layer, is_position_in_layer, user_transform, is_position_in_lod } from 'anymapper'
-    import { select, hover_enter, hover_leave } from 'anymapper'
+    import { select } from 'anymapper'
     import { Marker } from 'anymapper'
 
     export let data
@@ -13,14 +13,9 @@
 
 {#if visible}
 <g class="selectable" transform="translate({data.position.x},{data.position.y}) scale({1/$zoom})"
-    on:click={() => select(data.id)}
-    on:mouseenter={() => hover_enter(data.id)}
-    on:mouseleave={() => hover_leave(data.id)}>
+    on:click={() => select(data.id)}>
     <Marker
         icon={data.icon}
-        icon_spacing={data.icon_spacing}
-        text={data.text}
-        shape={data.shape}
         bg_color="steelblue"
         shadow
     />
