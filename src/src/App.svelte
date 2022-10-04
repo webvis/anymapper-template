23e04b3b8b305c203046f67fe88cab9b5b865235
nitaku <matteo.abrate@gmail.com>
2022-10-04 11:54:10 +0200

    stores removed (more simple example)

--- src/src/App.svelte
+++ src/src/App.svelte
@@ -4,7 +4,7 @@
 	modify this file to create your application
 -->
 <script lang="ts">
-	import type Fuse from 'fuse.js'
+	import { onMount } from 'svelte'
 	
 	import type { Entity } from 'anymapper'
 	import { Anymap, View, Layer, FloorLayersCtrl, InlineSVG, InfoBox, InfoBoxHeader, OmniBox, ResultsBox } from 'anymapper'
@@ -13,36 +13,50 @@
 	// this template makes use of Svelte Material UI (same one that's used by anymapper)
 	import { Content } from '@smui/card'
 
+	// let's use the fuse.js library for client-side full-text searching
+	import type Fuse from 'fuse.js'
 
-	// store example
-	import { pois, search } from './stores'
+	// example data
+	import { fetch_pois, index_pois } from './data'
 
+	let pois: Map<string, Entity> = new Map() // empty Map
+	let search_index: Fuse<Entity>
+	let results: Array<Fuse.FuseResult<Entity>> = []
+	
 	// component examples
 	import POI from './components/POI.svelte'
 	import Placemark from './components/Placemark.svelte'
 	import Results from './components/Results.svelte'
 
-	let results: Array<Fuse.FuseResult<Entity>> = []
-
 	let omnibox
 	let results_box
 
-	// what to do whenever there's a change of selected ID or pois
+	// update the selection whenever the selected ID changes
+	// (also update it whenever the underlying data - pois - is changed)
 	$: {
-		$selection = $pois.has($selected_id) ? $pois.get($selected_id) : null
+		$selection = pois.has($selected_id) ? pois.get($selected_id) : null
 	}
 
-	// what to do whenever the user issues a search
+	// update the search index whenever the data is changed
+	$: {
+		search_index = index_pois(pois)
+	}
+
+	// update results whenever the user issues a query
 	function handleSearch(e) {
-		results = search(e.detail.query)
+		results = search_index.search(e.detail.query)
 	}
 
-	// what to do whenever an entity is selected
-	// if there's a valid selection...
+	// whenever a valid entity is selected...
 	$: if($selection) {
 		// ...clear search results
 		results = []
 	}
+
+	// when the App is ready, fetch some data
+	onMount(async function () {
+		pois = await fetch_pois()
+	})
 </script>
 
 <style>
@@ -70,7 +84,7 @@
 
 		<!-- this layer displays the content of the $pois store using POI components -->
 		<Layer name="pois">
-			{#each Array.from($pois.values()) as poi}
+			{#each Array.from(pois.values()) as poi}
 				<POI entity={poi}/>
 			{/each}
 		</Layer>
---
+++ src/src/data.ts
@@ -0,0 +1,30 @@
+// example data
+// -------------
+// a simple collection of points of interest (poi) read from a JSON file
+// the collection is exposed as a JS Map (to support id-based lookup)
+// the collection is also indexed with fuse.js in order to support full-text searching
+
+import type { Entity } from 'anymapper'
+import Fuse from 'fuse.js'
+
+// read the list of pois from JSON, and return them as a Map of Entities
+export async function fetch_pois(): Promise<Map<string, Entity>> {
+    let response = await fetch('data/pois.json')
+    let data = await response.json()
+    let entities: Array<Entity> = data.map(d => ({...d, position: {...d.position, layers: new Set(d.position.layers)}})) // TBD: is Set necessary? if so, move it to a Point constructor
+    return new Map(entities.map(d => [d.id, d] ))
+}
+
+// index the given pois with fuse.js
+export function index_pois(pois: Map<string, Entity>): Fuse<Entity> {
+    const fuse: Fuse<Entity> = new Fuse(Array.from(pois.values()), {
+        ignoreLocation: true,
+        threshold: 0.3,
+        keys: [
+            "title",
+            "subtitle",
+            "content"
+        ]
+    })
+    return fuse
+}
